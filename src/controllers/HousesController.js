73177e1f2c23f8482f8dd5ec1540a76bfc649191
BlueFyre22 <bluefyr22@gmail.com>
2025-06-17 16:40:38 -0600

    query search and sort work now.

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -8,9 +8,36 @@ export class HousesController extends BaseController {
     super('api/houses')
     this.router
       .get("", this.getAllHouses)
-    // .get("/:id", this.getHouseById)
-    // .get("/search", this.getHousesByQuery)
+      .get("/search", this.getHousesByQuery) // search can be anything as long as you use it in postman
+      .get("/:houseId", this.getHouseById)
 
+  }
+
+
+
+
+  async getAllHouses(request, response, next) {
+    try {
+      const houses = await housesService.getAllHouses()
+      response.send(houses)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+   * @param {import("express").Request} request,
+   * @param {import("express").Response} response,
+   * @param {import("express").NextFunction} next,
+   */
+
+  async getHouseById(request, response, next) {
+    try {
+      const houseId = request.params.houseId
+      const house = await housesService.getHouseById(houseId)
+      response.send(house)
+    } catch (error) {
+      next(error)
+    }
   }
 
   /**
@@ -19,10 +46,13 @@ export class HousesController extends BaseController {
    * @param {import("express").NextFunction} next,
    */
 
-  async getAllHouses(request, response, next) {
+  async getHousesByQuery(request, response, next) {
     try {
-      const houses = await housesService.getAllHouses()
-      response.send(houses)
+      const houseQuery = request.query
+      console.log(houseQuery)
+      const house = await housesService.getHouseByQuery(houseQuery)
+      response.send(house)
+
     } catch (error) {
       next(error)
     }
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,3 +1,4 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 import { dbContext } from "../db/DbContext.js"
 
 
@@ -8,7 +9,35 @@ class HousesService {
     return house
   }
 
+  async getHouseById(houseId) {
+    const house = await dbContext.Houses.findById(houseId)
+
+    if (house == null) {
+
+      throw new BadRequest(`No House found with that ID sucka ${houseId}`)
+
+    }
+
+    return house
+
+  }
+
+  async getHouseByQuery(houseQuery) {
+
+    const sortBy = houseQuery.sort // .sort here is referencing what is passed via postman
+    delete houseQuery.sort
+
+    const houses = await dbContext.Houses.find(houseQuery).sort(sortBy)
+    return houses
+
+  }
+
+
+
+
 }
+
+
 
 
 export const housesService = new HousesService()
